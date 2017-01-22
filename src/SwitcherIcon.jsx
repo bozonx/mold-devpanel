147b3ea0ee50ec68502f7ecc09e7677ca0d5c20a
ivan <ipkozyrin@gmail.com>
2017-01-22 12:32:57 +1000

    change style

--- src/SwitcherIcon.jsx
+++ src/SwitcherIcon.jsx
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react';
 export default class SwitcherIcon extends React.Component {
   render() {
     return (
-      <div id="mold-devpanel-switcher">
+      <div className="mold-devpanel-switcher">
         {this.props.icon == 'open' && <span>&lsaquo;</span>}
         {this.props.icon == 'close' && <span>&rsaquo;</span>}
       </div>

82fd6079a7407779afeae9745e2fc78cacd3608f
ivan <ipkozyrin@gmail.com>
2017-01-24 22:44:15 +1000

    add

--- src/moldStruct/DocumentsCollection.jsx
+++ src/moldStruct/DocumentsCollection.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import _ from 'lodash';
 
 import ItemWrapper from '../ItemWrapper';
-import StructDocument from './Document';
+import Document from './Document';
 
 
 export default class DocumentsCollection extends React.Component {
@@ -24,36 +24,45 @@ export default class DocumentsCollection extends React.Component {
   componentWillMount() {
   }
 
-  _renderPages(pages, actionName) {
-    return _.map(pages, (page, index) => {
-      return <ItemWrapper name={`page${index}`}>
-        {this._renderCollection(page, actionName, index)}
-      </ItemWrapper>;
-    });
-  }
-
   _renderCollection(collection, actionName, pageNum) {
     return _.map(collection, (item, index) => {
       const moldPath = `${this.props.moldPath}[${item.$id}]`;
       const storagePath = `action.${actionName}[${pageNum}][${index}]`;
       return <ItemWrapper name={index}>
-        <StructDocument moldPath={moldPath}
-                        mold={this.props.mold}
-                        storage={_.get(this.storage, storagePath)} />
+        <Document moldPath={moldPath}
+                  mold={this.props.mold}
+                  storage={_.get(this.storage, storagePath)} />
       </ItemWrapper>;
     });
   }
 
+  _renderDocuments(name) {
+    const moldPath = `${this.props.moldPath}.${name}`;
+    return <ItemWrapper name={name}>
+      <Document moldPath={moldPath}
+                mold={this.props.mold} />
+    </ItemWrapper>;
+  }
+
+  _renderPages(pages, actionName) {
+    return <ItemWrapper name={actionName}>
+      {_.map(pages, (page, index) => {
+        return <ItemWrapper name={`page${index}`}>
+          {this._renderCollection(page, actionName, index)}
+        </ItemWrapper>;
+      })}
+    </ItemWrapper>;
+  }
 
   render() {
     const actionNames = _.keys(this.storage.action).sort();
+    const documentsNames = _.keys(this.storage.documents).sort();
     return (
       <div>
-        {_.map(actionNames, (name) => {
-          return <ItemWrapper name={name}>
-            {this._renderPages(this.storage.action[name], name)}
-          </ItemWrapper>;
-        })}
+        {_.map(actionNames, (name) => this._renderPages(this.storage.action[name], name))}
+        <ItemWrapper name="documents">
+          {_.map(documentsNames, (name) => this._renderDocuments(name))}
+        </ItemWrapper>
       </div>
     );
   }
